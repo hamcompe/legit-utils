c49e5f72988e6497010855bc950b91bdad07054a
Ham <pallop.chao@gmail.com>
2018-12-05 00:05:57 +0700

    Add min/max array

---
+++ arrMax.js
@@ -0,0 +1,4 @@
+const arrMax = arr => Math.max(...arr)
+// arrMax([20, 10, 5, 10]) -> 20
+
+module.exports = arrMax
---
+++ arrMin.js
@@ -0,0 +1,4 @@
+const arrMin = arr => Math.min(...arr)
+// arrMin([20, 10, 5, 10]) -> 5
+
+module.exports = arrMin
